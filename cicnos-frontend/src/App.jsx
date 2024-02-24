a8624f2b915355b73cc7b551723508f5bab63ff1
Umiterr <37944964+Umiterr@users.noreply.github.com>
2024-02-23 18:16:37 -0600

    add cards

--- cicnos-frontend/src/App.jsx
+++ cicnos-frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-
 import "./App.css";
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Header></Header>
-      <LandingPage></LandingPage>
+      <Header />
+      <LandingPage />
+      <img
+        src="../public/logo.svg"
+        alt="Descripción de la imagen"
+        className="BG_logo"
+      />
     </>
   );
 }
--- cicnos-frontend/src/components/Header.jsx
+++ cicnos-frontend/src/components/Header.jsx
@@ -33,13 +33,13 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white flex">
+    <header className="bg-white flex bg-opacity-0 relative z-20">
       <div className="flex lg:flex-1">
         <a href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
           <img
-            className="h-40 w-auto"
-            src="../public/logocicnos.jpg"
+            className=" w-auto"
+            src="../public/logocicnos.png"
             alt="Cicnos impresion en telas"
           />
         </a>
